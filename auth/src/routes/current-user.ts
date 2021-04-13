334a51ee15d1c2ad0e1bde93478090d5826b150e
Aaron <akhan727@gmail.com>
2021-04-13 17:24:54 -0600

    Created current user middleware to extract the JWT payload and set it on req.currentUser for so it can be used by other middlewares or the actual request handler.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,28 +1,11 @@
-import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import express from 'express';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  
-  // If user doesn't have 'req.session.jwt' set or JWT is invalid, return early
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // Validate payload and send back to client
-  try {
-    // jwt.verify(token,secretOrPrivateKey,[options,callback])
-    const payload = jwt.verify(
-      req.session.jwt, 
-      process.env.JWT_KEY!
-    );
-    res.send({ currentUser: payload });
-  } catch (err) {
-    // If JWT has been tampered with, verify() will throw an error
-    // This try-catch-block will be used to catch that possible error
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser ?? null });
 });
 
 export { router as currentUserRouter };
