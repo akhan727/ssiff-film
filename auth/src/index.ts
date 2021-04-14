5d6d836fadf925d8e2e791abef25c2f8caabb365
Aaron <akhan727@gmail.com>
2021-04-14 17:40:12 -0600

    Created a MONGO_URI environment variable. Added variable to auth-depl.yaml file.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,24 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
 
 const start = async () => {
-  // Checks if jwt key environment variable is defined
+  console.log('Starting up...');
+
+  // Checks if following environment variables are defined
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!');
   }
-  
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI environment variable must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
-    console.log('Connected to MongoDB...!')
+    console.log('Connected to MongoDB...!');
   } catch (err) {
     console.error(err);
   }
