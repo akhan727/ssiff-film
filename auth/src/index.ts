0a99ab9d35deadafae22d5b737b4a99e1b97ff58
Aaron <akhan727@gmail.com>
2021-04-14 12:42:05 -0600

    Refractored index.ts file in the auth directory. Added an app.ts file to handle app configuration. Added dev dependencies for jest, supertest, and mongodb-memory-server.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-// Middlewares (express)
-const app = express();
-app.set('trust proxy', true); // Tells Express to trust Ingress-Nginx proxy
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-);
-
-// Route Handlers
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-// Middlewares (auth)
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   // Checks if jwt key environment variable is defined
