cbb7611402e4999a4d4cc4efe56ef76be1091d04
Aaron <akhan727@gmail.com>
2021-06-11 18:50:22 -0600

    Added to debugger to films module

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -3,43 +3,6 @@ import { app } from '../../app';
 
 describe('/api/user/signin', () => {
   const endpoint = '/api/users/signin';
-  /*
-  it('returns a 400 with an invalid email', async () => {
-    const { body } = await request(app)
-      .post(endpoint)
-      .send({ email: 'invalid email', password: 'password' })
-      .expect(400);
-
-    expect(body).toHaveProperty('errors');
-    expect(body.errors).toHaveLength(1);
-    expect(body.errors[0]).toHaveProperty('field');
-    expect(body.errors[0].field).toEqual('email');
-  });
-
-  it('returns a 400 with an empty password', async () => {
-    const { body } = await request(app)
-      .post(endpoint)
-      .send({ email: 'test@test.com', password: '' })
-      .expect(400);
-
-    expect(body).toHaveProperty('errors');
-    expect(body.errors).toHaveLength(1);
-    expect(body.errors[0]).toHaveProperty('field');
-    expect(body.errors[0].field).toEqual('password');
-  });
-
-  it('returns a 400 with missing email or password', async () => {
-    await request(app)
-      .post(endpoint)
-      .send({ email: 'test@test.com' })
-      .expect(400);
-
-    await request(app)
-      .post(endpoint)
-      .send({ password: 'password' })
-      .expect(400);
-  });
-  */
 
   it('fails when an email that does not exist is supplied', async () => {
     const { body } = await request(app)
